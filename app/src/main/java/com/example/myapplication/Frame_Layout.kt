7f9bc493bbfbe7f882cf74c54ce0019787af73d4
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-06-27 20:36:09 +0700

    Next and Previous Function

--- app/src/main/java/com/example/myapplication/Frame_Layout.kt
+++ app/src/main/java/com/example/myapplication/Frame_Layout.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_frame__layout.*
 
@@ -24,6 +25,46 @@ class Frame_Layout : AppCompatActivity() {
             R.id.wonyoung2 ->{
                 wonyoung2.visibility = View.GONE
                 wonyoung.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    var i:Int = 0
+    fun next(view: View){
+        i++
+        when(i){
+            1 ->{
+                sakura.visibility = View.GONE
+                chaeyon.visibility = View.VISIBLE
+                editsearch.text ="2"
+            }
+            2 ->{
+                chaeyon.visibility = View.GONE
+                yuri.visibility = View.VISIBLE
+            }
+            3 -> {
+                yuri.visibility = View.GONE
+                sakura.visibility = View.VISIBLE
+                i -= 3
+            }
+        }
+    }
+    var j:Int = 0
+    fun pre(view: View){
+        j++
+        when(j){
+            1 ->{
+                sakura.visibility = View.GONE
+                yuri.visibility = View.VISIBLE
+            }
+            2 ->{
+                yuri.visibility = View.GONE
+                chaeyon.visibility = View.VISIBLE
+            }
+            3 -> {
+                chaeyon.visibility = View.GONE
+                sakura.visibility = View.VISIBLE
+                j -=3
             }
         }
     }
