417ebf267c1d202730cac5baa16499d07712b90b
Serey Vatanak <koeurnsereyvatanak18@kit.edu.kh>
2019-06-27 15:13:04 +0700

    Added more pictures

--- app/src/main/java/com/example/myapplication/Frame_Layout.kt
+++ app/src/main/java/com/example/myapplication/Frame_Layout.kt
@@ -2,11 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_frame__layout.*
 
 class Frame_Layout : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame__layout)
+    }
+
+    fun switchImage(view: View){
+        var imgView = view as ImageView
+
+        when(imgView.id){
+            R.id.wonyoung ->{
+                wonyoung.visibility = View.GONE
+                wonyoung2.visibility = View.VISIBLE
+            }
+            R.id.wonyoung2 ->{
+                wonyoung2.visibility = View.GONE
+                wonyoung.visibility = View.VISIBLE
+            }
+        }
     }
 }
